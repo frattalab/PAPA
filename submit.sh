4010c776023627917c1d1e2855977a8e7da88300
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2021-08-17 09:04:11 +0100

    scripts for ucl cluster submit

---
+++ submit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Submit to the cluster, give it a unique name
+#$ -S /bin/bash
+
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1.9G,h_rt=20:00:00,tmem=1.9G
+#$ -pe smp 2
+
+# join stdout and stderr output
+#$ -j y
+#$ -R y
+
+
+if [ "$1" != "" ]; then
+    RUN_NAME="PAPA"
+else
+    RUN_NAME=$1
+fi
+
+FOLDER=submissions/$(date +"%Y%m%d%H%M")
+
+mkdir -p ${FOLDER}
+cp config/config.yaml ${FOLDER}/${RUN_NAME}_config.yaml
+
+snakemake \
+-p \
+--use-conda \
+--jobscript cluster_qsub.sh \
+--cluster-config config/cluster.yaml \
+--cluster-sync "qsub -l tmem={cluster.tmem},h_vmem={cluster.h_vmem},h_rt={cluster.h_rt} -o $FOLDER {cluster.submission_string}" \
+-j 25 \
+--nolock \
+--rerun-incomplete \
+--latency-wait 50
