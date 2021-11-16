8e4335582994bac8b1ac19b74f03fc91b36f871a
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2021-11-16 14:09:11 +0000

    output matrix of polyA isoform TPMs

--- scripts/tx_to_polya_quant.R
+++ scripts/tx_to_polya_quant.R
@@ -15,20 +15,20 @@ option_list <- list(make_option(c("-s", "--sample-table"),
                     make_option(c("-g","--tx2gene"),
                                 type="character",
                                 help = "Path to <prefix>'.tx2pas.tsv file storing transcript ID to Gene ID assignment output by assign_tx_to_pas.py"),
-                    make_option(c("-o", "--output-file"),
-                                dest = "output_file",
-                                default = "summarised_pas_quantification.tsv",
-                                help = "Name of output TSV file storing summarise per-poly(A) site quantification ([default= %default])")
+                    make_option(c("-o", "--output-prefix"),
+                                dest = "output_prefix",
+                                default = "summarised_pas_quantification",
+                                help = "Prefix to names of output files storing per-poly(A) site summarised counts (<output_prefix>.counts.tsv) or TPM values (<output_prefix>.tpm.tsv) ([default= %default])")
                     )
 
 opt_parser <- OptionParser(option_list = option_list)
-opt <-parse_args(opt_parser)
-
 
 if (length(commandArgs(trailingOnly = TRUE)) == 0) {
   print_help(opt_parser)
   stop()
 }
+
+opt <- parse_args(opt_parser)
 
 #1. Read in sample table & construct paths to all Salmon quantification files
 
@@ -66,8 +66,11 @@ txi.tx <- tximport(quant_paths,
                    tx2gene = tx2gene,
                    countsFromAbundance = "lengthScaledTPM")
 
+#Sum abundance for transcripts sharing the same polyA site
 txi.pas <- summarizeToGene(txi.tx, tx2pas, countsFromAbundance = "lengthScaledTPM")
 
+# print(head(txi.pas$abundance))
+# print(head(txi.pas$counts))
 
 
 # Add 'poly(A) site IDs to counts df
@@ -76,8 +79,11 @@ isoform_id_col <- colnames(tx2pas)[-1]
 message(paste("isoform ID column:", isoform_id_col, sep=" "))
 
 pas_counts <- cbind(rownames(txi.pas$counts), as.data.frame(txi.pas$counts))
+pas_tpm <- cbind(rownames(txi.pas$abundance), as.data.frame(txi.pas$abundance))
 # print(head(pas_counts))
+
 colnames(pas_counts) <- c(isoform_id_col, colnames(as.data.frame(txi.pas$counts)))
+colnames(pas_tpm) <- c(isoform_id_col, colnames(as.data.frame(txi.pas$abundance)))
 
 # print(head(pas_counts))
 # Return gene ID to to counts df (so have polyASite ID & gene IDs to group events)
@@ -88,7 +94,7 @@ colnames(pas_counts) <- c(isoform_id_col, colnames(as.data.frame(txi.pas$counts)
 # print("tx2pas")
 # print(head(tx2pas))
 
-message("Adding gene ID to counts df...")
+message("Adding gene ID to counts & TPM df...")
 
 iso2gene <- merge(tx2gene, tx2pas, by = "transcript_id")[,c("gene_id", isoform_id_col)]
 # print(head(iso2gene))
@@ -99,21 +105,35 @@ pas_counts <- merge(pas_counts,
                     iso2gene,
                     by = isoform_id_col)
 
+pas_tpm <- merge(pas_tpm,
+                 iso2gene,
+                 by = isoform_id_col)
+
 # print(head(pas_counts))
 
 # reorder so pas_id | gene_id are the first two columns
 pas_counts <- pas_counts[, c(1, ncol(pas_counts), seq(2, ncol(pas_counts) - 1, 1))]
+pas_tpm <- pas_tpm[, c(1, ncol(pas_tpm), seq(2, ncol(pas_tpm) - 1, 1))]
 
 # Just in case, remove duplicate pas_id rows from matrix
 pas_counts <- pas_counts[!duplicated(pas_counts[, isoform_id_col]), ]
+pas_tpm <- pas_tpm[!duplicated(pas_tpm[, isoform_id_col]), ]
 
 # "data/tximport_pas_quantification.tsv"
-message(paste("Writing transcript counts file to -", opt$output_file, sep = " "))
+message(paste("Writing poly(A) isoform counts file to - ", opt$output_prefix, ".counts.tsv", sep = ""))
 
 write.table(pas_counts,
-            file = opt$output_file,
+            file = paste(opt$output_prefix, ".counts.tsv", sep=""),
             sep = "\t",
             quote = FALSE,
             col.names = TRUE,
             row.names = FALSE)
 
+message(paste("Writing poly(A) isoform tpm file to - ", opt$output_prefix, ".tpm.tsv", sep = ""))
+
+write.table(pas_tpm,
+            file = paste(opt$output_prefix, ".tpm.tsv", sep = ""),
+            sep = "\t",
+            quote = FALSE,
+            col.names = TRUE,
+            row.names = FALSE)
