2e4ed7a3e3b4e8521254ff7d5f0bf2ce678f925e
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2022-03-04 17:23:40 +0000

    fix bug with ','-sep novel ref_gene_ids strings being treated as separate genes to ref. Output info df with event types, coordinates, annot status etc.

--- scripts/get_combined_quant_gtf.py
+++ scripts/get_combined_quant_gtf.py
@@ -157,6 +157,139 @@ def update_extension_le_number(df, id_col="ref_gene_id", out_col="le_number_ext"
     return df
 
 
+def update_ext_le_ids(gr,
+                      type_col="event_type",
+                      le_ext_key="last_exon_extension",
+                      id_col="ref_gene_id",
+                      number_col="le_number",
+                      le_id_col="le_id"):
+    '''
+    '''
+
+    gr = gr.assign("is_extension",
+                   lambda df: pd.Series(np.where(df[type_col].str.contains(le_ext_key,
+                                                                           na=False,
+                                                                           regex=False),
+                                                 1,
+                                                 0
+                                                 )
+                                        )
+                   )
+
+
+    # Extra sort so extension events come after non-extensions of the same le_id
+    gr = gr.apply(lambda df:
+                  df.sort_values(by=[id_col, number_col, "is_extension"])
+                  )
+
+    # Adds column 'le_number_ext'
+    gr = gr.apply(lambda df: update_extension_le_number(df))
+
+    # eprint(combined_ext[["ref_gene_id", "le_id", "le_number", "event_type", "is_extension", "le_number_ext"]].print(n=50))
+
+    # Reassign le_id with updated number, return to same shape as no extensions gr
+    gr = (gr.drop([le_id_col, number_col, "is_extension"])
+            .apply(lambda df: df.rename(columns={"le_number_ext": number_col}))
+            .assign(le_id_col,
+                    lambda df: df[id_col] + "_" + df[number_col].astype(int).astype(str)
+                    )
+          )
+
+    return gr
+
+
+def annotate_le_ids(novel_le, ref_le, novel_id_col="gene_id_ref", ref_id_col="gene_id", le_id_outcol="le_id"):
+    '''
+    '''
+
+    # Identify last exon Ids containing novel extensions - these need to be handled separately
+    # set of cluster IDs for each chr, strand tuple
+    # {(chr, strand): {cluster_IDs}}
+    d_ext_gene_ids = novel_le.apply(lambda df: set(df[df["event_type"].str.contains("extension", regex=False)][novel_id_col]) # should be ref gene ID in novel events (foun)
+                                  ,
+                                  as_pyranges=False)
+
+    # Combine into a single set
+    # https://blog.finxter.com/union-multiple-sets-in-python/
+    ext_gene_ids = set().union(*d_ext_gene_ids.values())
+
+    # Separate novel & ref grs into genes with novel extensions ('<ref/novel>_le_ext') & those without ('<ref/novel>_le_n_ext')
+
+    ref_le_ext = ref_le.subset(lambda df: df[ref_id_col].isin(ext_gene_ids))
+    ref_le_n_ext = ref_le.subset(lambda df: ~(df[ref_id_col].isin(ext_gene_ids)))
+
+
+    novel_le_ext = novel_le.subset(lambda df: df[novel_id_col].isin(ext_gene_ids))
+    novel_le_n_ext = novel_le.subset(lambda df: ~(df[novel_id_col].isin(ext_gene_ids)))
+
+
+    # Make combined GTF of reference and novel last exons
+    # Keeping extensions separate
+    combined_ext = pr.concat([ref_le_ext, novel_le_ext])
+    combined_n_ext = pr.concat([novel_le_n_ext, ref_le_n_ext])
+
+    # Make sure all dfs of gr have same columns (number & labels)
+    # This *should* be the case, but if chr/strand df is unique to one of the concatenated grs then can see different num of cols
+    combined_ext = check_concat(combined_ext)
+    combined_n_ext = check_concat(combined_n_ext)
+
+
+
+
+
+    # make sure gene_id corresponds to reference gene ID
+    # Otherwise novel + ref of same gene will be considered separately (annotated in diff column)
+    # New common col = "ref_gene_id"
+
+    if len(combined_ext) > 0:
+
+        combined_ext = add_common_gene_id_col(combined_ext)
+        combined_ext = combined_ext.cluster(by="ref_gene_id")
+
+        # Assign 5'-3' 1..n 'last_exon number' for each gene
+        # Group together overlapping exons with a common identifier
+        # .cluster(strand=None) groups as ('I') expect i.e. only overlapping intervals on the same strand can be merged
+        combined_ext = cluster_to_region_number(combined_ext, "ref_gene_id")
+
+        eprint("assigning 'le_id' (last exon ID) for each gene...")
+
+        combined_ext = combined_ext.assign(le_id_outcol,
+                                           lambda df: df["ref_gene_id"] + "_" + df["le_number"].astype(int).astype(str)
+                                           )
+
+        # No extensions last exon number is ready to go
+        # Extension events - need to update le number so extension = own event (for last_extensions)
+        combined_ext = update_ext_le_ids(combined_ext,
+                                         type_col="event_type",
+                                         le_ext_key="last_exon_extension",
+                                         id_col="ref_gene_id",
+                                         le_id_col=le_id_outcol)
+
+
+    combined_n_ext = add_common_gene_id_col(combined_n_ext)
+
+    combined_n_ext = combined_n_ext.cluster(by="ref_gene_id")
+
+    combined_n_ext = cluster_to_region_number(combined_n_ext, "ref_gene_id")
+
+    eprint("assigning 'le_id' (last exon ID) for each gene...")
+
+    combined_n_ext = combined_n_ext.assign("le_id",
+                                           lambda df: df["ref_gene_id"] + "_" + df["le_number"].astype(int).astype(str)
+                                           )
+
+    if len(combined_ext) > 0:
+        # GTF containing defined last exons (with le_id etc. defined)
+        combined = pr.concat([combined_ext, combined_n_ext])
+
+    else:
+        combined = combined_n_ext
+
+    return combined
+
+
+
+
 def main(novel_le_path,
          ref_gtf_path,
          ref_attr_key_order,
@@ -197,138 +330,56 @@ def main(novel_le_path,
     # novel_le = pr.read_gtf(novel_le_path)
     novel_le = read_gtf_specific(novel_le_path, ["gene_id", "transcript_id", "exon_number", "gene_id_ref", "Start_ref", "End_ref", "event_type"])
 
+
+    # Make sure gene_id_ref has a single value string if only 1 distinct gene ID (Otherwise collapse to non-redundant comma-separated string of gene IDs)
+    # TODO: looping over Series is a dirty way to do this, find a more canonical approach
+    novel_le = novel_le.assign("gene_id_ref",
+                               lambda df: pd.Series([",".join(list(dict.fromkeys(ref_id.split(","))))
+                                                     for ref_id in df["gene_id_ref"]])
+                               )
+
+    # These events need to be handled separately - will create a 'metagene' combining
+    eprint(f"Number of novel events matching multiple reference genes - {novel_le.as_df()['gene_id_ref'].str.contains(',', regex=False).sum()}")
+
+    # Split novel_le into novel_le_single (overlaps 1 ref gene ID) & novel_le_mult (overlaps > 1 ref gene ID)
+    # These will be annotated separately then combined in the final step
+    novel_le_single = novel_le.subset(lambda df: ~df['gene_id_ref'].str.contains(',', regex=False))
+    novel_le_mult = novel_le.subset(lambda df: df['gene_id_ref'].str.contains(',', regex=False))
+
+    # Need a set of ref gene_ids from novel_le_mult to subset reference GTF
+    # Present in novel_le_mult like <gene_id>,<gene_id_b> - need to split by 'comma' and expand into sinlge list
+    mult_ref_ids = set(novel_le_mult.as_df()['gene_id_ref'].str.split(',').explode())
+
+    # Split ref le into _single & _mult
+    ref_le_single = ref_le.subset(lambda df: ~df['gene_id'].isin(mult_ref_ids))
+    ref_le_mult = ref_le.subset(lambda df: df['gene_id'].isin(mult_ref_ids))
+
+    # Need to update the reference gene_id for _mult events
+    # in novel - <gene_id_a>,<gene_id_b>
+    # in ref - <gene_id_a>
+    # create dict of {<ref_id_1>: <ref_id_1>,<ref_id_2>, <ref_id_2>: <ref_id_1>,<ref_id_2>}
+    mult_ids_dict = {}
+    for mult_id in novel_le_mult.as_df()['gene_id_ref']:
+        splt = mult_id.split(",")
+        for id in splt:
+            mult_ids_dict[id] = mult_id
+
+    ref_le_mult = ref_le_mult.assign("gene_id",
+                                     lambda df: df["gene_id"].apply(lambda x: mult_ids_dict[x]))
+
+    # eprint(ref_le_mult[["gene_id"]])
+
+    # Combine across ref & novel, annotate le_ids
+    # Note that novel extension events are annotated separately, as these will overlap with their reference counterpart (so need to update assigned le_number)
     eprint("Combining ref & novel last exons objects and grouping last exons based on overlap...")
-
-    # Identify last exon Ids containing novel extensions - these need to be handled separately
-    # set of cluster IDs for each chr, strand tuple
-    # {(chr, strand): {cluster_IDs}}
-    d_ext_gene_ids = novel_le.apply(lambda df: set(df[df["event_type"].str.contains("extension", regex=False)]["gene_id_ref"]) # should be ref gene ID in novel events (foun)
-                                                ,
-                                    as_pyranges=False)
-
-    # eprint(d_ext_gene_ids)
-    #
-    # for k,v in d_ext_gene_ids.items():
-    #     eprint(k)
-    #     eprint(d_ext_gene_ids[k])
-
-    # Combine into a single set
-    # https://blog.finxter.com/union-multiple-sets-in-python/
-    ext_gene_ids = set().union(*d_ext_gene_ids.values())
-
-    ref_le_ext = ref_le.subset(lambda df: df["gene_id"].isin(ext_gene_ids))
-    ref_le_n_ext = ref_le.subset(lambda df: ~(df["gene_id"].isin(ext_gene_ids)))
-
-    # eprint("Reference genes containing novel extensions")
-    # eprint(ref_le_ext)
-    #
-    # eprint("Reference genes without novel extensions")
-    # eprint(ref_le_n_ext)
-
-    novel_le_ext = novel_le.subset(lambda df: df["gene_id_ref"].isin(ext_gene_ids))
-
-    # eprint("Genes with novel last exon (extensions)")
-    # eprint(novel_le_ext)
-
-    # eprint(novel_le.columns)
-
-    novel_le_n_ext = novel_le.subset(lambda df: ~(df["gene_id_ref"].isin(ext_gene_ids)))
-
-    # eprint("Genes with novel last exon (no extensions)")
-    # eprint(novel_le_n_ext)
-
-    # eprint(novel_le_n_ext.columns)
-    # eprint(novel_le_n_ext)
-
-
-    # Make combined GTF of reference and novel last exons
-    # Keeping extensions separate
-    combined_ext = pr.concat([ref_le_ext, novel_le_ext])
-    combined_n_ext = pr.concat([novel_le_n_ext, ref_le_n_ext])
-
-    # Make sure all dfs of gr have same columns (number & labels)
-    # This *should* be the case, but if chr/strand df is unique to one of the concatenated grs then can see different num of cols
-    combined_ext = check_concat(combined_ext)
-    combined_n_ext = check_concat(combined_n_ext)
-
-    # Group together overlapping exons with a common identifier
-    # .cluster(strand=None) groups as ('I') expect i.e. only overlapping intervals on the same strand can be merged
-    combined_ext = combined_ext.cluster()
-    combined_n_ext = combined_n_ext.cluster()
-
-    # eprint(combined_ext[["gene_id", "Cluster", "event_type"]])
-    # eprint(combined_ext.columns)
-    #
-    # eprint(combined_n_ext[["gene_id", "Cluster", "event_type"]])
-    # eprint(combined_n_ext.columns)
-
-    # make sure gene_id corresponds to reference gene ID
-    # Otherwise novel + ref of same gene will be considered separately (annotated in diff column)
-    combined_ext = add_common_gene_id_col(combined_ext)
-    combined_n_ext = add_common_gene_id_col(combined_n_ext)
-
-    # Assign 5'-3' 1..n 'last_exon number' for each gene
-    combined_ext = cluster_to_region_number(combined_ext, "ref_gene_id")
-
-    # DF blows up if I don't do this
-    # eprint(len(combined_ext))
-    # combined_ext = combined_ext.apply(lambda df: df.drop_duplicates(subset="transcript_id"))
-    # eprint(len(combined_ext))
-
-    combined_n_ext = cluster_to_region_number(combined_n_ext, "ref_gene_id")
-
-    eprint("assigning 'le_id' (last exon ID) for each gene...")
-
-    combined_ext = combined_ext.assign("le_id",
-                                       lambda df: df["ref_gene_id"] + "_" + df["le_number"].astype(int).astype(str)
-                                       )
-
-    combined_n_ext = combined_n_ext.assign("le_id",
-                                           lambda df: df["ref_gene_id"] + "_" + df["le_number"].astype(int).astype(str)
-                                           )
-
-    # eprint(len(combined_n_ext))
-    # combined_n_ext = combined_n_ext.apply(lambda df: df.drop_duplicates(subset="transcript_id"))
-    # eprint(len(combined_n_ext))
-
-    # No extensions last exon number is ready to go
-    # Extension events - need to update le number so extension = own event
-    combined_ext = combined_ext.assign("is_extension",
-                                       lambda df: pd.Series(np.where(df["event_type"].str.contains("last_exon_extension",
-                                                                                                   na=False,
-                                                                                                   regex=False),
-                                                                     1,
-                                                                     0
-                                                                     )
-                                                            )
-                                       )
-
-
-    # Extra sort so extension events come after non-extensions of the same le_id
-    combined_ext = combined_ext.apply(lambda df:
-                                      df.sort_values(by=["ref_gene_id", "le_number", "is_extension"])
-                                      )
-
-    combined_ext = combined_ext.apply(lambda df: update_extension_le_number(df))
-
-    # eprint(combined_ext[["ref_gene_id", "le_id", "le_number", "event_type", "is_extension", "le_number_ext"]].print(n=50))
-
-    # Reassign le_id with updated number, return to same shape as no extensions gr
-    eprint(combined_ext.columns)
-    combined_ext = (combined_ext.drop(["le_id", "le_number", "is_extension"])
-                    .apply(lambda df: df.rename(columns={"le_number_ext": "le_number"}))
-                    .assign("le_id",
-                            lambda df: df["ref_gene_id"] + "_" + df["le_number"].astype(int).astype(str)
-                            )
-                    )
-
-    # eprint(combined_ext)
+    combined_single = annotate_le_ids(novel_le_single, ref_le_single, le_id_outcol="le_id")
+    combined_mult = annotate_le_ids(novel_le_mult, ref_le_mult, le_id_outcol="le_id")
 
     # GTF containing defined last exons (with le_id etc. defined)
-    combined = pr.concat([combined_ext, combined_n_ext])
+    combined = pr.concat([combined_single, combined_mult])
 
     # Want a GTF containing 'unique regions' (relative to first/internal exons) for each last exon
-    # These regions will be used for quantification
+    # These regions will be used for quantification (don't want to assign expression in shared region only to the last exon (internal exons aren't included))
 
     eprint("Extracting unique regions for last exons overlapping reference first/internal exons")
     ref_e_nl = pr.concat([get_terminal_regions(ref_e, which_region="first"),
@@ -352,20 +403,22 @@ def main(novel_le_path,
 
     eprint(f"Writing 'tx2gene' (transcript_id | gene_id) to TSV... - {output_prefix + '.tx2gene.tsv'}")
 
-    (quant_combined.as_df()
-     [["transcript_id", "ref_gene_id"]]
-     .drop_duplicates()
-     .rename(columns={"ref_gene_id": "gene_id"})
-     .to_csv(output_prefix + ".tx2gene.tsv",
-                               sep="\t",
-                               index=False,
-                               header=True)
+    (quant_combined.subset(lambda df: df.duplicated(subset=["gene_id"], keep=False)) # remove single isoform genes (keep='False' marks all duplicates as True (so keep these))
+     .as_df()
+                   [["transcript_id", "ref_gene_id"]]
+                   .drop_duplicates()
+                   .rename(columns={"ref_gene_id": "gene_id"})
+                   .to_csv(output_prefix + ".tx2gene.tsv",
+                           sep="\t",
+                           index=False,
+                           header=True)
      )
 
 
-    eprint(f"Writing 'tx2le' (transcript_id | pas_id) to TSV... - {output_prefix + '.tx2le.tsv'}")
+    eprint(f"Writing 'tx2le' (transcript_id | le_id) to TSV... - {output_prefix + '.tx2le.tsv'}")
 
-    (quant_combined.as_df()
+    (quant_combined.subset(lambda df: df.duplicated(subset=["gene_id"], keep=False))
+     .as_df()
      [["transcript_id", "le_id"]]
      .drop_duplicates()
      .sort_values(by="le_id")
@@ -376,7 +429,8 @@ def main(novel_le_path,
      )
 
     eprint(f"Writing 'le2gene' (le_id | gene_id) to TSV... - {output_prefix + '.le2gene.tsv'}")
-    (quant_combined.as_df()
+    (quant_combined.subset(lambda df: df.duplicated(subset=["gene_id"], keep=False))
+     .as_df()
      [["le_id", "ref_gene_id"]]
      .drop_duplicates()
      .sort_values(by="le_id")
@@ -386,6 +440,25 @@ def main(novel_le_path,
              index=False,
              header=True)
      )
+
+    eprint(f"writing 'info' table (tx_id | le_id | gene_id | gene_name | event_type | coords | annot_status) to file - {output_prefix + '.info.tsv'}")
+    (quant_combined.subset(lambda df: df.duplicated(subset=["gene_id"], keep=False))
+     .as_df()
+     [["transcript_id", "le_id", "ref_gene_id", "gene_name", "event_type", "Chromosome", "Start", "End", "Strand"]]
+     .rename(columns={"ref_gene_id": "gene_id"})
+     .drop_duplicates()
+     .assign(**{"annot_status": lambda df: np.where(df["transcript_id"].str.contains("PAPA", regex=False),
+                                                    "novel",
+                                                    "annotated"),
+                }
+             )
+     .sort_values(by="le_id")
+     .to_csv(output_prefix + ".info.tsv",
+             sep="\t",
+             index=False,
+             header=True)
+
+    )
 
 
 
