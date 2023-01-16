ef0c0eab4eacbe7364388f10e53e8bec2515146a
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2023-01-16 15:44:25 +0000

    fix collapsing annotation info for le_ids bug. le_ids with inconsistent unique vals are not collapsed

--- scripts/process_saturn_tbl.R
+++ scripts/process_saturn_tbl.R
@@ -33,6 +33,9 @@ ppau_path <- opt$ppau
 output_prefix <- opt$output_prefix
 
 suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(purrr))
+
 
 # read in input files
 
@@ -64,6 +67,9 @@ info <- rename_with(info, tolower)
 # reorder cols so coordinate info comes at the end (just move annot_status before chromosome)
 info <- relocate(info, annot_status, .before = chromosome)
 
+# TODO: only interested in le_ids analysed by satuRn - should subset for these only
+info <- filter(info, le_id %in% pull(res, le_id))
+
 # The same le_id can have multiple transcripts (& multiple coords) contributing to it
 # Collapse these to a single row for each LE (saves the results table getting over-duplicated)
 
@@ -72,29 +78,62 @@ exp_uniq_cols <- c("gene_id", "gene_name", "event_type", "annot_status", "chromo
 # vector of colnames from info where expect le_id to have multiple distinct values across rows
 exp_collapse_cols <- c("transcript_id", "start", "end")
 
-# TODO: implement check for whether le_id does not have 1 value for exp_uniq_cols (if so separate, parse separately)
+# Check whether le_id does not have 1 value for exp_uniq_cols (if so separate, parse separately)
+# summarise(across(x, ~ f)) expects every element in x to produce an identical length/size output from the function f (I think)
+# If expected unique cols have > 2 different counts of unique values then will get an error with mismatched sizes
+# these le_ids will not have their values collapsed for now...
 
-# n_le_ids <- n_distinct(info$le_id)
-# info %>%
-#   group_by(le_id) %>%
-#   # for each le, count number of unique values in each col (expect = 1)
-#   summarise(across(all_of(exp_uniq_cols),
-#             n_distinct)
-#             )
+# NB: this is often caused by 'event_type', any cols where column values are collapsed if multiple annotated partner txs (e.g. 'gene_name').
+# TODO: consider updating how retain annotation information for novel events of output GTFs... (separate df?)
+# TODO: really expect event_type to be unique for a le_id (which is just combination of overlapping isoforms)?
 
-info_clpsd <- info %>%
+# For these le_ids, info rows will not be collapsed to a single row
+n_exp_uniq <- info %>%
+  group_by(le_id) %>%
+  # for each le, count number of unique values in each col (expect = 1)
+  summarise(across(all_of(exp_uniq_cols),
+            n_distinct),
+            ) %>%
+  # le_id | exp_uniq_cols ...
+  # Keep le_ids with > 1 value in uniq cols (potential troublemakers...)
+  filter(if_any(all_of(exp_uniq_cols), ~ .x != 1)) %>%
+  pivot_longer(cols = -le_id,
+               names_to = "col_name",
+               values_to = "n_uniq_vals") %>%
+  group_by(le_id) %>%
+  # Number of unique per-column unique value counts 
+  mutate(n = n_distinct(n_uniq_vals)) %>%
+  # If > 2 then parsing below will break
+  filter(n > 2)
+
+if (length(n_exp_uniq) != 0) {
+  problem_le_ids <- unique(n_exp_uniq$le_id)
+} else {
+  print("All expected columns have a single unique value for all le_ids")
+  problem_le_ids <- c()
+}
+
+info_gd <- filter(info, !le_id %in% problem_le_ids)
+info_bd <- filter(info,  le_id %in% problem_le_ids)
+
+info_clpsd_gd <- info_gd %>%
   group_by(le_id) %>%
   summarise(across(all_of(exp_uniq_cols),
                                unique),
             across(all_of(exp_collapse_cols),
-                   ~ paste(.x, collapse = ",")
-                   ),
-
+            ~ paste(.x, collapse = ","))
             )
 
 # move chromosome & strand back next to start & end coords
-info_clpsd <- relocate(info_clpsd, chromosome, strand, .before = start)
+# line up colnames before concatenating 
+# TODO: is lining up necessary?
+info_clpsd_gd <- relocate(info_clpsd_gd, chromosome, strand, .before = start)
+info_bd <- relocate(info_bd, transcript_id,  chromosome, strand, .before = start)
 
+# change start & end to characters 
+info_bd <- mutate(info_bd, across(.cols = all_of(c("start", "end")), as.character))
+
+info_clpsd <- bind_rows(info_clpsd_gd, info_bd)
 
 # now join info with the results tbl
 res_jned <- left_join(res, info_clpsd, by = "le_id")
@@ -112,6 +151,3 @@ write.table(x = res_jned,
             row.names = F, 
             quote = F
             )
-
-
-
